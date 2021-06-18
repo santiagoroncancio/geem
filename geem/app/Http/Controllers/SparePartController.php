108b71129992e0fe11a2496ef2e47be826e8ade2
Santiago <santiagoroncancio2108@gmail.com>
2021-06-18 16:57:00 -0500

    Controllers with class resources

---
+++ geem/app/Http/Controllers/SparePartController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Spare_part;
+use Illuminate\Http\Request;
+
+class SparePartController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Spare_part  $spare_part
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Spare_part $spare_part)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Spare_part  $spare_part
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Spare_part $spare_part)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Spare_part  $spare_part
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Spare_part $spare_part)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Spare_part  $spare_part
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Spare_part $spare_part)
+    {
+        //
+    }
+}
